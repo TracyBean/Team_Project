0bd68c31637c4b7470e5c572512ab15efdf1b399
Mike Bielagus <bielagus@gmail.com>
2013-07-14 14:08:44 -0400

    added geocoding and address to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
-  attr_accessible :date, :description, :name, :time
+  attr_accessible :date, :description, :name, :address, :time, :latitude, :longitude
+
+  geocoded_by: address
+  after_validation :geocode
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130714180750) do
+
+  create_table "events", :force => true do |t|
+    t.string   "name"
+    t.date     "date"
+    t.time     "time"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "address"
+  end
 
 end
