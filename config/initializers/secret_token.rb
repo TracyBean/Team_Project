4199b7962847febda9fec17999baa51c0b87d14e
Tracy Moscato <tracy@Tracys-MacBook-Air.local>
2013-07-14 13:15:44 -0400

    created Libboo added vincenzo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamProject::Application.config.secret_token = 'd8ab52d7c3cf0ab5ec07864a62c20e62ee72a022e3e353212c65425afbe5f99571468346cf3c79cc83e6d796f391e42fa880adefaadf9a458eacdc16390eb3a7'
